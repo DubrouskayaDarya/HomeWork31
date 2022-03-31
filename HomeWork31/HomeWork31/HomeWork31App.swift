3d749301a4abc9c83a0c5aee05f43989a8cf9a1e
Дарья Дубровская <>
2022-03-31 20:37:54 +0300

    Task 31: File added

---
+++ HomeWork31/HomeWork31/HomeWork31App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork31App.swift
+//  HomeWork31
+//
+//  Created by Дарья Дубровская on 31.03.22.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork31App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
